9a00191c62b907cb8b751a890d92868eae4ed24d
Omkar Patil <omkar.s.patil-1@ou.edu>
2018-02-26 12:21:09 -0500

    remove node_modules

--- angular-src/node_modules/ngx-toastr/toastr/toast-token.d.ts
+++
@@ -1,7 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { GlobalConfig } from './toastr-config';
-export interface ToastToken {
-    config: GlobalConfig;
-    defaults: any;
-}
-export declare const TOAST_CONFIG: InjectionToken<ToastToken>;
